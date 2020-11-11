9269aae656e90a1a3ce28fe9c69558363a050c2a
Angel Baez <51308340+abaez004@users.noreply.github.com>
2020-11-10 19:32:11 -0500

    Update BDM_HW4_Baez.py

--- BDM_HW4_Baez.py
+++ BDM_HW4_Baez.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
 		.builder\
 		.master("yarn")\
 		.appName("Consumer Complaints")\
-		.getOrCreate;
+		.getOrCreate();
 	
 	df = ss.read.csv(sys.argv[1], header=True, inferSchema=True, multiLine=True)
 	df.head(5) 
