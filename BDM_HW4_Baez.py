dc26c757b6e43c2ee5451d389665d5ff7192eca0
Angel Baez <51308340+abaez004@users.noreply.github.com>
2020-11-10 19:21:33 -0500

    Add files via upload

---
+++ BDM_HW4_Baez.py
@@ -0,0 +1,12 @@
+from pyspark import SparkSession
+import sys
+
+if __name__=='__main__':
+	ss = SparkSession\
+		.builder\
+		.master("yarn")\
+		.appName("Consumer Complaints")\
+		.getOrCreate;
+	
+	df = ss.read.csv(sys.argv[1], header=True, inferSchema=True, multiLine=True)
+	df.head(5) 
